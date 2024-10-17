114b12bd98a19dc7fbd73c1c39f9f385d45a5a6f
Yonatan Mateo Aviv <122502051+intercalaris@users.noreply.github.com>
2024-10-17 08:32:40 -0600

    updated key logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Shows the word and the definition
 document.querySelector('#search-button').addEventListener('click', searchWord);
+let authKey = '097e233f-0941-4845-b46a-31f58c7a6700:fx';
 
 function searchWord() {
     let word = document.querySelector('#word-input').value;
@@ -24,7 +25,6 @@ function searchWord() {
 document.querySelector('#show-german-word').addEventListener('click', showGermanWord);
 
 function showGermanWord() {
-    let authKey = '097e233f-0941-4845-b46a-31f58c7a6700:fx';
     let text = document.querySelector('#word-name').innerHTML;
     fetch(`https://api-free.deepl.com/v2/translate?auth_key=${authKey}&text=${text}&target_lang=de&formality=less`)
         .then(res => res.json())
@@ -39,9 +39,7 @@ function showGermanWord() {
 
 // Shows and translates the word in Spanish
 document.querySelector('#show-spanish-word').addEventListener('click', showSpanishWord);
-
 function showSpanishWord() {
-    let authKey = '097e233f-0941-4845-b46a-31f58c7a6700:fx';
     let text = document.querySelector('#word-name').innerHTML;
     fetch(`https://api-free.deepl.com/v2/translate?auth_key=${authKey}&text=${text}&target_lang=es&formality=less`)
         .then(res => res.json())
@@ -70,7 +68,7 @@ function getRandomWord() {
 
 // Search random word definition
 function searchRandomWord(word) {
-    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=2746d714-0448-476c-a27d-43544f77a767`)
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=APIKEY`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
